4e1422dc11e16136cab0c3bf4308fb9755790c84
AichaAO97 <aicha.ait.oumghar17@gmail.com>
2022-08-11 23:20:06 +0100

    update the todo.completed attributeckbox

--- app.py
+++ app.py
@@ -17,15 +17,14 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False )
+    completed = db.Column(db.Boolean, nullable = False, default = False)
 
     def __repr__(self) :
         return f'< Todo {self.id} {self.description} > '
 
 
 db.create_all()
-@app.route('/')
-def index():
-    return render_template('index.html', data = Todo.query.all())
+
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -49,3 +48,22 @@ def create_todo():
     if not error:
         return jsonify(body)
 
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+          db.session.rollback()
+    finally:
+         db.session.close()
+    return redirect(url_for('index'))
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data = Todo.query.all())
